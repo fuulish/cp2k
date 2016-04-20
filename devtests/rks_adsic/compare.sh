18dbe8148ce19eadd35c1713ee3331eb7aba793d
Frank Uhlig <uhlig.frank@gmail.com>
2016-04-20 17:53:18 +0200

    removed some print statements, test files not containing absolute paths, and comparing to reference energy from before and printing forces

--- devtests/rks_adsic/compare.sh
+++ devtests/rks_adsic/compare.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+refen=-1.181951039128841
 lsden=`grep 'ENERGY| ' lsd/test.out | awk '{print $9}'`
 rksen=`grep 'ENERGY| ' rks/test.out | awk '{print $9}'`
 
-result=`echo "scale=5; $lsden - $rksen" | bc`
+result=`echo "scale=5; $lsden - $refen" | bc`
 
 if [[ "$result" > "0.00001" ]]; then
     exit 1;
 else
-    echo "everything is fine"
-    exit 0;
+    echo "reference lsd is fine"
+
+    result=`echo "scale=5; $lsden - $rksen" | bc`
+
+    if [[ "$result" > "0.00001" ]]; then
+        exit 1;
+    else
+        echo "everything is fine"
+        exit 0;
+    fi
 fi
