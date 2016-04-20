899faef8aaff704c544a7983459127e34076b1ff
Frank Uhlig <uhlig.frank@gmail.com>
2016-04-20 20:02:23 +0200

    changed order of comparison, this way first test has a chance of not failing

--- devtests/rks_adsic/compare.sh
+++ devtests/rks_adsic/compare.sh
@@ -4,19 +4,19 @@ refen=-1.181951039128841
 lsden=`grep 'ENERGY| ' lsd/test.out | awk '{print $9}'`
 rksen=`grep 'ENERGY| ' rks/test.out | awk '{print $9}'`
 
-result=`echo "scale=5; $lsden - $refen" | bc`
+result=`echo "scale=5; $lsden - $rksen" | bc`
 
 if [[ "$result" > "0.00001" ]]; then
     exit 1;
 else
     echo "reference lsd is fine"
 
-    result=`echo "scale=5; $lsden - $rksen" | bc`
+    result=`echo "scale=5; $lsden - $refen" | bc`
 
     if [[ "$result" > "0.00001" ]]; then
         exit 1;
     else
-        echo "everything is fine"
+        echo "also conforming to LSD (PADE) reference energy"
         exit 0;
     fi
 fi
